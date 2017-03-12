16c4239b1fcf0fe22f07ab7d4c7977f2c6329388
Vineet <Vineet@Vineet-PC>
2017-03-12 18:39:55 +0530

    Test

--- JavaPractice/src/Test.java
+++ JavaPractice/src/Test.java
@@ -3,8 +3,21 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Test");
-
+		
+		StringBuilder sb = new StringBuilder("Hi");
+		System.out.println(sb);
+		
+		sb = null; 
+		// setting instance to null make object eligible for garbage collection
+		
+		
+		//Reassigning a Reference variable
+		StringBuilder s1 = new StringBuilder("s1");
+		StringBuilder s2 = new StringBuilder("s2");
+		
+		s1 = s2;
+		//Now StringBuilder("s1"); is eligible for garbage collection
+		
 	}
 
 }
