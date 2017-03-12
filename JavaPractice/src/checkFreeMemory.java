6af248bb6ad3d72e5014e19c26fc3e5c35f7462f
Vineet <Vineet@Vineet-PC>
2017-03-12 19:02:21 +0530

    Garbage collector

---
+++ JavaPractice/src/checkFreeMemory.java
@@ -0,0 +1,27 @@
+
+import java.util.Date;
+
+public class checkFreeMemory {
+
+	public static void main(String[] args) {
+		
+		Runtime rt = Runtime.getRuntime();
+		
+		System.out.println("Total memory: "+rt.totalMemory());
+		System.out.println("Free memory: "+rt.freeMemory());
+		
+		Date d1 = null;
+		
+		for(int i=0; i < 1000; i++ ){
+			d1 = new Date();
+			d1 = null;
+		}
+		
+		rt.gc();
+		
+		System.out.println("Total memory: "+rt.totalMemory());
+		System.out.println("Free memory: "+rt.freeMemory());
+
+	}
+
+}
