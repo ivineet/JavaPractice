62730c11899e9792ee4e2971f015dbb68fa4ddc5
koder <Vineet@Vineet-PC>
2017-03-20 22:46:19 +0530

    enum properties

--- JavaPractice/src/Enum1.java
+++ JavaPractice/src/Enum1.java
@@ -7,7 +7,21 @@
 
 import java.lang.Enum;
 
- //enum outside class
+ /*
+  * Note: 
+  * 1. An enum can be declared outside or inside a class, but NOT in a method
+  * 2. An enum declared outside a class must NOT be marked static, final,
+	abstract, protected, or private.
+  * 3. Enums can contain constructors, methods, variables, and constant class bodies.
+  * 4. enum constants can send arguments to the enum constructor, using the
+       syntax BIG(8), where the int literal 8 is passed to the enum constructor.
+  * 5. enum constructors can have arguments, and can be overloaded
+  * 6. enum constructors can NEVER be invoked directly in code. They are always
+       called automatically when an enum is initialized.
+  * 7. The semicolon at the end of an enum declaration is optional. These are legal:
+		enum Foo { ONE, TWO, THREE}
+		enum Foo { ONE, TWO, THREE};
+  */
 
 
 class Coffee{
