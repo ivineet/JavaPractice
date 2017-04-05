33d5f6145d8940cf3115cb76e6b5def919570dec
koder <Vineet@Vineet-PC>
2017-04-05 23:14:42 +0530

    Inner class

---
+++ JavaPractice/src/innerclasses/Outer1.java
@@ -0,0 +1,39 @@
+package innerclasses;
+
+//Inner class Example
+public class Outer1 {
+	private int ivar;
+
+	private class Inner { // inner class
+		private int num; // (could have an Inner() ctor)
+
+		private void foo() {
+			num++; // can access our regular inner ivar
+			ivar = 13; // we can "see" our outer class automatically
+			Outer1.this.ivar = 13; // same as above
+			System.out.println(num);
+		}
+
+		public String toString() {
+			return "Beat Cal";
+		}
+	}
+
+	public Object test() {
+		ivar = 10;
+		Inner in = new Inner();
+		in.foo(); // can see things, even if private
+		//in.toString(); // call an Object method
+		return in;
+		// Return pointer to inner to our caller as Object.
+		// They can call toString() on it.
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Outer1 ou1 = new Outer1();
+		System.out.println(ou1.test());
+
+	}
+
+}
