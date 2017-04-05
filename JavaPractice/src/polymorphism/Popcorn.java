3122cea1d09800e1046fcf8758c4756d457de1dc
koder <Vineet@Vineet-PC>
2017-04-06 00:10:43 +0530

    Polymorphism example

---
+++ JavaPractice/src/polymorphism/Popcorn.java
@@ -0,0 +1,29 @@
+package polymorphism;
+
+class Popcorn {
+	public void pop() {
+		System.out.println("popcorn");
+	}
+}
+
+class Food {
+	Popcorn p = new Popcorn() {
+		public void sizzle() {
+			System.out.println("anonymous sizzling popcorn");
+		}
+
+		public void pop() {
+			System.out.println("anonymous popcorn");
+		}
+	};
+
+	public void popIt() {
+		p.pop(); // OK, Popcorn has a pop() method
+		p.sizzle(); // Not Legal! Popcorn does not have sizzle()
+	}
+}
+
+/*
+ * Compiling the preceding code gives us something like, Anon.java:19: cannot
+ * resolve symbol symbol : method sizzle ()
+ */
